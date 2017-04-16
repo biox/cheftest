04c1463fb50a43523d40e8c673415fa5f43d7acd
Jesse Olson <jessebolson@icloud.com>
2017-04-16 11:46:23 -0500

    Added recipe code

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,11 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+file '/etc/motd' do
+  content 'This is Jesse Server leave now'
+  mode 0644
+  owner 'root'
+  group 'root'
+  action :create
+end
