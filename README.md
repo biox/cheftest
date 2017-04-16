250cb04b7883483cceedba2e9b89f77b26f62cc8
Jesse Olson <jessebolson@icloud.com>
2017-04-16 11:37:36 -0500

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# hp_cheftest
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: hp_cheftest
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
